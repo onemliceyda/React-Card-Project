8912c4d650579e189f6fb02f11abe6515429d5b1
Ceyda <ceydaonemli2@gmail.com>
2022-01-26 16:36:18 +0300

    class component added.

---
+++ src/components/Collapse.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const Collapse = (props) => {
+    return (
+        <div>
+            <a className="btn btn-primary w-100" data-bs-toggle="collapse" href={"#".concat(props.href)} role="button" aria-expanded="false" aria-controls="collapseExample">
+                Link with href
+            </a>
+            <div className="collapse show" id={props.href}>
+                {props.children}
+            </div>
+        </div>
+    );
+};
+export default Collapse;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
+//react'ın çalışmaya başladığı dosya.component yazıp,tarayıcıda göstereceğiz.
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Card from './components/Card';
+import Collapse from './components/Collapse';
+
+
+const App = () => {
+
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="card-group w-100">
+                    <div className="col">
+                        <Collapse href="collapseExample1">
+                            <Card cardTitle="Card Title 1"
+                                cardText="Lorem Ipsum Text1"
+                                updatedTime="Last updated 1 min ago"
+                                image="https://picsum.photos/id/1/200/300" />
+                        </Collapse>
+                    </div>
+
+                    <div className="col">
+                        <Collapse href="collapseExample2">
+                            <Card cardTitle="Card Title 2"
+                                cardText="Lorem Ipsum Text2"
+                                updatedTime="Last updated 2 min ago"
+                                image="https://picsum.photos/id/10/200/300" />
+                        </Collapse>
+                    </div>
+
+                    <div className="col">
+                        <Collapse href="collapseExample3">
+                            <Card cardTitle="Card Title 3"
+                                cardText="Lorem Ipsum Text3"
+                                updatedTime="Last updated 3 min ago"
+                                image="https://picsum.photos/id/100/200/300" />
+                        </Collapse>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
+
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
